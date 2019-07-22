81d5663f889532d9c79be91db02d9e7235d2344b
andy <thamandyy@gmail.com>
2019-07-22 18:26:45 -0400

    Dependencies and setup

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+	mode: "development",
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: './',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test:/\.(s*)css$/,
+        use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+				]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif|ico)$/,
+        use: [{
+            loader: 'url-loader',
+            options: {
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+            }
+        }]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html',
+      open: false
+    }
+  },
+  plugins: [
+		new MiniCssExtractPlugin(
+      {
+        filename: "style.css"
+      }
+    ),
+		new HtmlWebpackPlugin(
+			{
+				hash: false,
+				template: "./index.html"
+			}
+		)
+  ]
+};
+
+module.exports = config
